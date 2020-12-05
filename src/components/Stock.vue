f613c4a43303cafeade31b90897ecfa16e92fe6a
lichencarel20 <747277763@qq.com>
2020-12-05 14:44:57 +0800

    complete the part of stock typeOf pie

--- src/components/Stock.vue
+++ src/components/Stock.vue
@@ -12,6 +12,8 @@ export default {
     return {
       chartInstance: null,
       allData: null,
+      currentIndex: 0,
+      timer: null, //定时器标识
     };
   },
   watch: {},
@@ -21,58 +23,172 @@ export default {
     this.initChart();
     this.getData();
     window.addEventListener("resize", this.screenAdapter);
-    this.screenAdapter;
+    this.screenAdapter();
   },
   destroyed() {
     window.removeEventListener("resize", this.screenAdapter);
+    clearInterval(this.timer);
   },
   methods: {
     initChart() {
-      this.chartInstance = this.$echarts.init(this.$refs.stock_ref);
-      const initOption = {};
+      this.chartInstance = this.$echarts.init(this.$refs.stock_ref, "chalk");
+      const initOption = {
+        title: {
+          text: "| 库存和销量分析",
+          left: "5%",
+          top: "5%",
+        },
+      };
       this.chartInstance.setOption(initOption);
+      this.chartInstance.on("mouseover", () => {
+        clearInterval(this.timer);
+      });
+      this.chartInstance.on("mouseout", () => {
+        this.startInterval();
+      });
     },
     //获取数据
     async getData() {
       const { data } = await this.$http.get("stock");
       console.log(data, "data");
-      this.allData=data
+      this.allData = data;
       this.updateChart();
+      this.startInterval();
     },
     //处理图表需要的数据
+
     updateChart() {
       //饼图所在的位置数据
-      const centerArr=[
-        ["18%","40%"],
-        ["50%","40%"],
-        ["82%","40%"],
-        ["34%","75%"],
-        ["66%","75%"],
-      ]
-      const showData=this.allData.slice(0,5)
-      const seriesArr=showData.map((item,index)=>{
+      const centerArr = [
+        ["18%", "40%"],
+        ["50%", "40%"],
+        ["82%", "40%"],
+        ["34%", "75%"],
+        ["66%", "75%"],
+      ];
+
+      const colorArr = [
+        ["#4FF778", "#0BA82C"],
+        ["#E5DD45", "#E8B11C"],
+        ["#E8821C", "#E55445"],
+        ["#5052EE", "#AB6EE5"],
+        ["#23E5E5", "#2E72BF"],
+      ];
+
+      const start = this.currentIndex * 5;
+      const end = (this.currentIndex + 1) * 5;
+      const showData = this.allData.slice(start, end);
+      const seriesArr = showData.map((item, index) => {
         return {
-          type:'pie',
-          radius:[110,100],
-          center:centerArr[index],
-          data:[{
-            value:item.sales
-          },{
-            value:item.stock
-          }]
-        }
-      })
-      const dataOption={
-        series:seriesArr
-      }
-      this.chartInstance.setOption(dataOption)
+          type: "pie",
+          radius: [110, 100],
+          center: centerArr[index],
+          hoverAnimation: false, //鼠标动画效果的移除
+          labelLine: {
+            show: false, //隐藏指示线
+          },
+          label: {
+            position: "center", //位置的设置
+            color: colorArr[index][0], //LABEL的颜色设置
+          },
+          data: [
+            {
+              name: item.name + "\n" + item.sales,
+              value: item.sales,
+              itemStyle: {
+                color: new this.$echarts.graphic.LinearGradient(0, 1, 0, 0, [
+                  {
+                    offset: 0,
+                    color: colorArr[index][0],
+                  },
+                  {
+                    offset: 0,
+                    color: colorArr[index][1], //圆环颜色的设置
+                  },
+                ]),
+              },
+            },
+            {
+              // name:'hs',
+              value: item.stock,
+              itemStyle: {
+                color: "#333843",
+              },
+            },
+          ],
+        };
+      });
+      const dataOption = {
+        series: seriesArr,
+      };
+      this.chartInstance.setOption(dataOption);
     },
 
-    screenAdapter(){
-      const adapterOption={}
-      this.chartInstance.setOption(adapterOption)
-      this.chartInstance.resize()
-    }
+    screenAdapter() {
+      const titleFontSize = this.$refs.stock_ref.offsetWidth / 100* 3.6;
+      // console.log(titleFontSize)
+      const innerRadius = titleFontSize * 2;
+      const outterRadius = innerRadius * 1.125;
+      const adapterOption = {
+        title: {
+          textStyle: {
+            fontSize: titleFontSize,
+          },
+        },
+        series: [
+          {
+            type: "pie",
+            radius: [outterRadius, innerRadius],
+            label: {
+              fontSize: titleFontSize/2,
+            },
+          },
+          {
+            type: "pie",
+            radius: [outterRadius, innerRadius],
+            label: {
+              fontSize: titleFontSize/2,
+            },
+          },
+          {
+            type: "pie",
+            radius: [outterRadius, innerRadius],
+            label: {
+              fontSize: titleFontSize/2,
+            },
+          },
+          {
+            type: "pie",
+            radius: [outterRadius, innerRadius],
+            label: {
+              fontSize: titleFontSize/2,
+            },
+          },
+          {
+            type: "pie",
+            radius: [outterRadius, innerRadius],
+            label: {
+              fontSize: titleFontSize/2,
+            },
+          },
+        ],
+      };
+      this.chartInstance.setOption(adapterOption);
+      this.chartInstance.resize();
+    },
+
+    startInterval() {
+      if (this.timer) {
+        clearInterval(this.timer);
+      }
+      this.timer = setInterval(() => {
+        if (this.currentIndex == 1) {
+          this.currentIndex = -1;
+        }
+        this.currentIndex++;
+        this.updateChart(); //更改数据后需要调用刷新
+      }, 5000);
+    },
   },
 };
 </script>
